e3bdf7eff5c3e8a2098c6066f6f8fed6201943e8
Andrés Velasco <velasco.andrs@gmail.com>
2021-03-25 21:27:24 -0500

    add more files

---
+++ lib/sign_up/cubit/sign_up_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:formz/formz.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  SignUpCubit() : super(SignUpInitial());
+}
